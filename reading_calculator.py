f5d72cac0b8ed8f2cfac4ee9dc4ed2bbc329de86
Anna Karingal <anna.karingal@gmail.com>
2015-11-21 12:37:11 -0500

    fix syntax errors

--- reading_calculator.py
+++ reading_calculator.py
@@ -22,14 +22,17 @@ class ReadingCalculator:
             # dial hand is between numbers
             # go with the lower number unless between 0 and 9:
             if dial.between_values: 
-                val = (dial.between_0_and_9 ? 9 : min(dial.values))
+                if not dial.between_0_and_9: 
+                    val = min(dail.values)
+                else: 
+                    val = 9
 
             # dial hand is directly on a number: look at dial on right (if any)
             # if value of dial on right is between 0 and 7, set value to 
             # one less than value
             else:
                 val = dial.values[0]
-                if not idx == len(dials)-1 && reading_vals[-1] >= 7: 
+                if not idx == len(dials)-1 and reading_vals[-1] >= 7: 
                         val = dial.values[0] - 1
 
             reading_vals.append(val);
