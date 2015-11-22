e3fce2d34131a4f0c6f1b242f4e36f15db503ee5
Anna Karingal <anna.karingal@gmail.com>
2015-11-22 11:17:27 -0500

    find and display all circles -- works for first image

--- image.py
+++ image.py
@@ -17,7 +17,32 @@ class Image:
         img = cv2.imread(fname)
         width, length, colors = img.shape
         if width > length: 
-            w, l = IMG_DIMENSIONS
+            l, w= IMG_DIMENSIONS
         else: 
-            l,w = IMG_DIMENSIONS
+            w, l= IMG_DIMENSIONS
         return cv2.resize(img, (w,l))
+
+    def find_dials(self, **kwargs):
+        # Get kwargs
+        dp = kwargs.get("dp", 20)
+        min_d = kwargs.get("min_dist", 30)
+        min_r = kwargs.get("min_radius", 25)
+        max_r = kwargs.get("max_radius", 60)
+
+        # Find possible dials
+        possible_dials = cv2.HoughCircles(self.gray, cv2.cv.CV_HOUGH_GRADIENT, dp, min_d,
+                        minRadius=min_r, maxRadius=max_r)
+
+        if possible_dials is not None: 
+            possible_dials = np.round(possible_dials[0, :]).astype('int')
+            output = self.img.copy()
+            for (x,y,r) in possible_dials: 
+                print (x,y,r)
+                cv2.circle(output, (x,y), r, (0,255,0), 4)
+                cv2.rectangle(output, (x-5, y-5), (x+5, y+5), (0, 127, 255), -1)
+            print len(possible_dials)
+            cv2.imshow('output', np.hstack([self.img, output]))
+            cv2.waitKey(0)
+
+        else: 
+            print "No dials found"
--- main.py
+++ main.py
@@ -6,3 +6,4 @@ ap.add_argument('-i', '--image', required=True, help='Path to image')
 cmd_args = vars(ap.parse_args())
 
 img = Image(image_fname=cmd_args['image'])
+dials = img.find_dials()
