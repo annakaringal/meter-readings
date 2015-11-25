98f92f1dac62c123fd252710cd8ed9d5990ab63d
Anna Karingal <anna.karingal@gmail.com>
2015-11-25 16:09:39 -0500

    calculate needle orientation using orientation of ellipse of best fit

--- dial.py
+++ dial.py
@@ -21,8 +21,12 @@ class Dial:
         gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
         return cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 19, 2)
 
-    def between_values(self):
-        return self.lval != self.rval
+    def needle_orientation(self):
+        (contours, _h) = cv2.findContours(self.threshold.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        conts = sorted(contours, key = cv2.contourArea, reverse=True)[:1]
+        self.ellipse = cv2.fitEllipse(conts[0])
+        (self.xe,self.ye),(self.MA, self.ma), self.orientation = self.ellipse
+        return self.orientation - 90
 
     def between_0_and_9(self):
         return (0 in self.values() and 9 in self.values())
