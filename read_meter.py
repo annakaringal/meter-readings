6ddf9edcb1b0731bfcef12668bff83d82da28fe1
Anna Karingal <anna.karingal@gmail.com>
2015-11-25 16:29:34 -0500

    pass in dial template image from cmd line to Dial instance init

--- read_meter.py
+++ read_meter.py
@@ -10,6 +10,7 @@ from reading_calculator import calculate_reading
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help='Path to image')
 ap.add_argument('-d', '--dials', required=False, default='4', help='Number of dials in image')
+ap.add_argument('-t', '--template', required=False, default='template/dial.jpg', help='Template of dial ')
 cmd_args = vars(ap.parse_args())
 
 # Get image from command line and make a copy for output visuals
@@ -19,6 +20,9 @@ output = meter_img.image().copy()
 # Find positions of dials in image
 dial_properties = meter_img.find_dials(num_dials=int(cmd_args['dials']))
 
+# Read in the template image for the dial needle
+template = cv2.imread(cmd_args['template'], 0)
+
 dials = []
 for (x,y,r) in dial_properties: 
     # Draw each dial on output image
@@ -26,8 +30,7 @@ for (x,y,r) in dial_properties:
 
     # Create new Dial instance, determine position of hand
     # and add to list of dials
-    d = Dial(center=[x,y],radius=r)
-    d.determine_hand_position(meter_img)
+    d = Dial(center=[x,y], radius=r, image=meter_img, dial_template=template)
     dials.append(d)
 
 # Calculate and print meter reading to console
